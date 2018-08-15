56ec3235698625e202dd25e62484d49cfba913a4
Matthew Crooks <mfcrooks@gmail.com>
2018-08-15 00:39:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 function findMatching(list, name) {
   return list.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
-  })
+  });
+}
+
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.legnth
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
