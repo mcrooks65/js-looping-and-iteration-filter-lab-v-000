40e5411326c3ceb032d8a68a1cda0cc3d2ea179e
Matthew Crooks <mfcrooks@gmail.com>
2018-08-15 00:40:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function findMatching(list, name) {
 }
 
 function fuzzyMatch (list, partialName) {
-  let lengthOfName = partialName.legnth;
+  let lengthOfName = partialName.length;
   return list.filter(function (driverName) {
     return driverName.slice(0, lengthOfName) === partialName;
   });
